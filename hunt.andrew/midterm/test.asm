c59b0233938a8a3e5a8a64f03337a977f8a044ea
Raymond Zheng <raymond.zheng@mymail.champlain.edu>
2022-12-06 12:40:14 -0500

    assembly env setup

---
+++ hunt.andrew/midterm/test.asm
@@ -0,0 +1,16 @@
+; First Program for MASM 32-bit
+.386
+.MODEL FLAT, stdcall
+.STACK 4096
+ExitProcess PROTO, dwExitCode:DWORD
+.DATA
+num DWORD 80
+sum DWORD ?
+.CODE
+_main PROC
+   mov eax, num
+   add eax, 20
+   mov sum, eax
+   INVOKE ExitProcess, 0
+_main ENDP
+END
